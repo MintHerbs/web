2ffb52908e33a2f1c29c9f9215708b18455243dd
MintHerbs <hexanians@gmail.com>
2025-04-17 13:18:45 +0400

    updated

--- db.php
+++ db.php
@@ -2,11 +2,11 @@
 $host = 'localhost';
 $db = 'korisnici';
 $user = 'root';
-$pass = ''; // prazno ako ne koristiš lozinku u XAMPP-u
+$pass = ''; // blank if no password
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Konekcija neuspešna: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>

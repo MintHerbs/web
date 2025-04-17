1265b002adac21ed356539486d510c1d24b6a008
MintHerbs <hexanians@gmail.com>
2025-04-17 13:28:15 +0400

    db connect

--- db.php
+++ db.php
@@ -2,11 +2,19 @@
 $host = 'localhost';
 $db = 'korisnici';
 $user = 'root';
-$pass = ''; // blank if no password
+$pass = '';  // XAMPP’s default root password is empty
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// (Optionally) ensure you’re using the right character set:
+$conn->set_charset('utf8mb4');
+
+// …your queries go here…
+
+// when you’re done:
+$conn->close();
 ?>
